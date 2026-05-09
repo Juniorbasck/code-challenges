a3fcd4ce4e4ddd57254f737540c4fe72ad94f7be
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2026-05-09 17:13:08 -0300

    feat: add WithMostWater solution and implementation

---
+++ Medium/WithMostWater/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        int[] height = new[] { 1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+        var result = WithMostWater(height);
+        
+        Console.WriteLine("The result is: " + result);
+    }
+
+    public static int WithMostWater(int[] height)
+    {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxArea = 0;
+
+        while (left < right)
+        {
+            int width = right - left;
+            int currentHeight = Math.Min(height[right], height[left]);
+            var currentArea = width * currentHeight;
+
+            if (currentArea > maxArea)
+            {
+                maxArea = currentArea;
+            }
+
+            if (height[left] < height[right])
+            {
+                left++;   
+            }
+            else
+            {
+                right--;
+            }
+
+        }
+        
+        return maxArea;
+    }
+}
