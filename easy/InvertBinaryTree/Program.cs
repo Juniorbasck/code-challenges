31db3817bf2ecf3424c61ab47eff36e75fb68101
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2026-05-21 19:52:54 -0300

    feat: add InvertBinaryTree project with initial implementation

---
+++ easy/InvertBinaryTree/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Solution
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public static void Main()
+    {
+        var tree = new TreeNode(4,
+            new TreeNode(2, 
+                new TreeNode(1), 
+                new TreeNode(3)
+            ),
+            new TreeNode(7, 
+                new TreeNode(6), 
+                new TreeNode(9)
+            )
+        );
+        var result = InvertTree(tree);
+        
+    }
+
+    public static TreeNode InvertTree(TreeNode root)
+    {
+        if(root == null)
+            return null;
+                
+        var temp = root.left;
+        
+        root.left = root.right;
+        root.right = temp;
+        
+        InvertTree(root.left);
+        InvertTree(root.right);
+
+        return root;
+    }
+}
---
+++ easy/ValidAnagram/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Solution {
+    public static void Main()
+    {
+
+        string firstWord = "anagram";
+        string secondWord = "nagaram";
+        var result = IsAnagram(firstWord,secondWord);
+        Console.WriteLine(result);
+    }
+
+    public static bool IsAnagram(string s, string t)
+    {
+        var firstWord = s.ToArray();
+        var secondWord = t.ToArray();
+        
+        // if (sizeFirst != sizeSecond) return false;
+
+        var letterConters = new Dictionary<char, int>();
+
+        foreach (char latter in firstWord)
+        {
+            if (!letterConters.ContainsKey(latter))
+                letterConters.Add(latter, 1);
+    
+            letterConters[latter]++;        
+        }
+
+        foreach (char latter in secondWord)
+        {
+            if (!letterConters.ContainsKey(latter))
+            {
+                return false;
+            }
+            
+            letterConters[latter]--;
+        }
+
+
+        return true;
+    }
+}
